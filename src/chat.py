ab99134c829a07870f9f34eade974a3134aee6c9
artem-p <artempugachev1@gmail.com>
2016-12-13 13:07:25 +0300

    delete stopwords

--- src/chat.py
+++ src/chat.py
@@ -1,11 +1,13 @@
 request_type_cur_weather = 1
 import nltk
 import string
+from nltk.corpus import stopwords
 
 def get_request_info_by_message(message):
     """
     Parse chat message. Make url to weather service from it.
     :param message:
+
     :return: type: request type constant, url: str
     """
     cur_weather_default = 'http://178.62.201.176/api/1.0/current'
@@ -24,7 +26,12 @@ def tokenize(message):
     """
     tokens = nltk.word_tokenize(message)
 
-    # удаляем пунктуацию
+    # delete punctuations
     tokens = [token for token in tokens if (token not in string.punctuation)]
+
+    # delete stopwords
+    stop_words = stopwords.words('russian')
+    stop_words.extend(['что', 'это', 'так', 'вот', 'быть', 'как', 'в', 'к', 'на'])
+    tokens = [token for token in tokens if (token not in stop_words)]
 
     return tokens
